662fc5fad5c1a9d527104e95460c5cd7c1faff7b
tehilaami <159727184+tehilaami@users.noreply.github.com>
2024-02-15 18:05:50 +0200

    commit

--- server.js
+++ server.js
@@ -48,5 +48,5 @@ app.post("/api/buy", async (req, res) => {
 
 const PORT = 3000
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
+    console.log(`Server is running on https://localhost:${PORT}`)
 })
