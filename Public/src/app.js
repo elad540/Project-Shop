8766abe232a9ce4e0b4b8c1fd7fcac9d3930e8ed
tehilaami <159727184+tehilaami@users.noreply.github.com>
2024-02-22 00:31:24 +0200

    update

--- Public/src/app.js
+++ Public/src/app.js
@@ -2,24 +2,24 @@
 
 const gProducts = [
   {
-    id: 1, 
+    id: 1,
     name: "A Couch",
     price: 2000,
   },
   {
-    id: 2, 
+    id: 2,
     name: "Coffee Table",
     price: 50,
-  },{
-    id: 3, 
+  }, {
+    id: 3,
     name: "Dining Table",
     price: 1500,
-  },{
-    id: 4, 
+  }, {
+    id: 4,
     name: "Kitchen Island",
     price: 1200,
-  },{
-    id: 5, 
+  }, {
+    id: 5,
     name: "Outdoor Sofa",
     price: 500,
   },
@@ -34,91 +34,75 @@ function renderProducts() {
   document.querySelector(".prodsList").innerHTML = strHTMLS.join("")
 }
 
-// Function to handle user login
 async function login(event) {
   try {
     event.preventDefault();
-    // Extract username and password from the login form
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    // Check for empty fields
     if (username === "" || password === "") return;
 
-    // Send login request to server
     const response = await fetch("/api/signin", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username, password }),
     });
 
-    // Parse server response
     const data = await response.json();
 
-    // Handle unsuccessful login
     if (!data.success) {
       alert(data.message);
       return;
     }
 
-    // Successful login: store user information and redirect to home page
     const loggedInUser = data.user;
     storageService.setUser(loggedInUser);
-    window.location.href = "/products.html"; // Redirect to product selection page
+    window.location.href = "/products.html"; 
   } catch (error) {
     console.log(error);
     alert(error.message);
   }
 }
 
-// Function to handle user signup
 async function signup(event) {
   try {
     event.preventDefault();
-    // Extract username, password, and email from the signup form
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
     const email = document.getElementById("email").value;
 
-    // Check for empty fields
     if (username === "" || password === "" || email === "") {
       alert("Please fill in all fields.");
       return;
     }
 
-    // Create credentials object
     const credentials = { username, password, email };
 
-    // Send signup request to server
     const response = await fetch("/api/signup", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(credentials),
     });
 
-    // Parse server response
     const data = await response.json();
 
-    // Redirect to login page after successful signup
     window.location.href = "/products.html";
   } catch (error) {
     console.log(error);
   }
 }
 
-// Function to initialize the application
 function init() {
-  // Retrieve user information from local storage
   const user = storageService.getUser();
 
-  // Redirect to login page if user is not logged in
+  
   if (!user) {
     window.location.href = "/signin.html";
     return;
   }
- renderProducts()
+  renderProducts()
 }
-// Function to add a product to the selected list
+
 function addToCart(productName, productPrice, productId) {
   const selectedProducts = storageService.getSelectedProducts();
   const newCartItem = {
@@ -131,15 +115,13 @@ function addToCart(productName, productPrice, productId) {
   alert(`Product '${productName}' added to the cart!`);
 }
 
-// Function to handle user logout
+
 function logout() {
-  // Clear user and selected products information from local storage
   storageService.clearAll();
-  // Redirect to login page
   window.location.href = "/signin.html";
 }
 
-// Finding products by name
+
 function searchProducts() {
   var searchInput = document.getElementById('searchInput').value.toLowerCase();
   var products = document.querySelectorAll('.product');
@@ -155,52 +137,46 @@ function searchProducts() {
 }
 
 function displayCheckoutInfo() {
-    console.log("displayCheckoutInfo called");
-    const selectedProducts = storageService.getSelectedProducts();
-    let totalAmount = 0;
-    let totalProducts = 0;
+  console.log("displayCheckoutInfo called");
+  const selectedProducts = storageService.getSelectedProducts();
+  let totalAmount = 0;
+  let totalProducts = 0;
 
-    // Display selected products
-    const selectedProductsContainer = document.getElementById("selectedProductsContainer");
-    selectedProductsContainer.innerHTML = ""; // Clear previous content
+  
+  const selectedProductsContainer = document.getElementById("selectedProductsContainer");
+  selectedProductsContainer.innerHTML = ""; 
 
-    selectedProducts.forEach(product => {
-        const productDiv = document.createElement("div");
-        productDiv.textContent = `${product.name} - ₪${(+product.price).toFixed(2)}`;
-        selectedProductsContainer.appendChild(productDiv);
+  selectedProducts.forEach(product => {
+    const productDiv = document.createElement("div");
+    productDiv.textContent = `${product.name} - ₪${(+product.price).toFixed(2)}`;
+    selectedProductsContainer.appendChild(productDiv);
 
-        // Calculate total price
-        totalAmount += +product.price;
+    totalAmount += +product.price;
 
-        // Calculate total products
-        totalProducts += 1;
-    });
+    totalProducts += 1;
+  });
 
-    // Display total amount and total products
-    const totalAmountElement = document.getElementById("totalAmount");
-    const totalProductsElement = document.getElementById("totalProducts");
-    totalAmountElement.textContent = `₪${totalAmount.toFixed(2)}`;
-    totalProductsElement.textContent = totalProducts;
+  const totalAmountElement = document.getElementById("totalAmount");
+  const totalProductsElement = document.getElementById("totalProducts");
+  totalAmountElement.textContent = `₪${totalAmount.toFixed(2)}`;
+  totalProductsElement.textContent = totalProducts;
 }
 
 async function confirmPurchase() {
   try {
     const selectedProducts = storageService.getSelectedProducts();
 
-    // Check if there are any selected products
     if (selectedProducts.length === 0) {
       alert("No products selected for purchase.");
       return;
     }
 
-    // Fetch user information from local storage
     const user = storageService.getUser();
     if (!user) {
       alert("User not logged in. Please log in before making a purchase.");
       return;
     }
 
-    // Prepare data for the purchase request
     const purchaseData = {
       userId: user._id,
       products: selectedProducts.map(product => ({
@@ -209,7 +185,6 @@ async function confirmPurchase() {
       })),
     };
 
-    // Send purchase request to the server
     const response = await fetch("/api/products", {
       method: "POST",
       headers: {
@@ -218,19 +193,15 @@ async function confirmPurchase() {
       body: JSON.stringify(purchaseData),
     });
 
-    // Parse server response
     const data = await response.json();
 
-    // Handle unsuccessful purchase
     if (!data.success) {
       alert("Purchase failed. Please try again.");
       return;
     }
 
-    // Clear selected products after successful purchase
     storageService.setSelectedProducts([]);
 
-    // Redirect to the product selection page
     window.location.href = "/products.html";
   } catch (error) {
     console.log(error);
@@ -238,6 +209,8 @@ async function confirmPurchase() {
   }
 }
 
+
 function toBuy() {
-  window.location.href("/buy.html")
+  storageService.clearAll();
+  window.location.href = "/buy.html";
 }
--- Public/src/storageService.js
+++ Public/src/storageService.js
@@ -1,35 +1,27 @@
 "use strict";
-// This file contains the functions for app.js
 
-// Constants representing keys for local storage
 const USER_KEY = "loggedInUser";
 const SELECTED_PRODUCTS_KEY = "selectedProducts";
 
-// Object encapsulating methods for interacting with local storage
 const storageService = {
-  // Retrieve the selected products from local storage, or an empty array if not present
   getSelectedProducts() {
     const selectedProducts = JSON.parse(localStorage.getItem(SELECTED_PRODUCTS_KEY));
     return selectedProducts || [];
   },
 
-  // Store the provided selected products in local storage
   setSelectedProducts(products) {
     localStorage.setItem(SELECTED_PRODUCTS_KEY, JSON.stringify(products));
   },
 
-  // Retrieve user information from local storage, or null if not present
   getUser() {
     const user = JSON.parse(localStorage.getItem(USER_KEY));
     return user || null;
   },
 
-  // Store the provided user information in local storage
   setUser(user) {
     localStorage.setItem(USER_KEY, JSON.stringify(user));
   },
 
-  // Remove both user and selected products information from local storage
   clearAll() {
     localStorage.removeItem(USER_KEY);
     localStorage.removeItem(SELECTED_PRODUCTS_KEY);
